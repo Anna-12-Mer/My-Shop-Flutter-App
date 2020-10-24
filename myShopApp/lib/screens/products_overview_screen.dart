191605322cd9e5b998245de0029ff6276fbfaee8
Anna-12-Mer <mammeramna@gmail.Com>
2020-10-24 16:07:01 +0200

    first commit

--- myShopApp/lib/screens/products_overview_screen.dart
+++ myShopApp/lib/screens/products_overview_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import '../models/products.dart';
+
 class ProductsOverviewScreen extends StatelessWidget {
-  const ProductsOverviewScreen({Key key}) : super(key: key);
+  final List<Product> loadedProducts = [];
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-
-    );
+    return Container();
   }
 }
